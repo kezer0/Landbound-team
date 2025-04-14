bed99c8b71f45199f8ac8293ce1409481187c945
kezer0 <kezer0gg@gmail.com>
2025-04-14 20:07:53 +0200

    fix: fixed interactions whit water

--- src/main/java/me/kezer0/landbound/utils/boundaryListener.java
+++ src/main/java/me/kezer0/landbound/utils/boundaryListener.java
@@ -5,7 +5,11 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class boundaryListener implements Listener {
 
@@ -14,22 +18,38 @@ public class boundaryListener implements Listener {
         Player player = event.getPlayer();
         Location loc = player.getLocation();
         World world = player.getWorld();
-
-        // Sprawdzamy czy to jest świat gracza
         if (!world.getName().equalsIgnoreCase(player.getUniqueId().toString())) return;
 
-        // Sprawdzamy czy stoi w wodzie
         Block block = loc.getBlock();
         if (block.getType() != Material.WATER) return;
 
-        // Teleportujemy na najwyższy bezpieczny blok na środku wyspy (0, ?, 0)
-        Location center = new Location(world, 0, 0, 0);
+        Location center = new Location(world, 8, 0, 8);
         int highestY = world.getHighestBlockYAt(center);
-        center.setY(highestY + 1); // 1 ponad najwyższy blok
+        center.setY(highestY + 1);
         center.setPitch(player.getLocation().getPitch());
         center.setYaw(player.getLocation().getYaw());
 
         player.teleport(center);
         player.sendMessage("Nie możesz wchodzić do wody! Przenoszę Cię na wyspę.");
+    }
+    @EventHandler
+    public void onWaterSpread(BlockFromToEvent event) {
+        World world = event.getBlock().getWorld();
+        if(world.getName().equals("world") || world.getName().equals("world_nether") || world.getName().equals("world_end")) return;
+        if (event.getBlock().getType() == Material.WATER) {
+            event.setCancelled(true);
+        }
+    }
+    @EventHandler
+    public void onPlayerBucketFill(PlayerBucketFillEvent e) {
+        Player player = e.getPlayer();
+        if (e.getBlockClicked().getType() == Material.WATER) {
+            if (player.getInventory().getItemInMainHand().getType() == Material.BUCKET) {
+                ItemStack waterBucket = new ItemStack(Material.WATER_BUCKET);
+                player.getInventory().setItemInMainHand(waterBucket);
+                e.getBlockClicked().setType(Material.WATER);
+                e.setCancelled(true);
+            }
+        }
     }
 }
